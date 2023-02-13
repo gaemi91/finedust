51cd7a658576d75bbfe34ae6ca4edf99d3e7ee83
gaemi91_apple <lepoete91@gmail.com>
2023-02-14 02:19:26 +0900

    Add DataTime Function

--- lib/my_app/my_app.dart
+++ lib/my_app/my_app.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MyApp{
+class MyApp {
   static ValueNotifier<bool> valueNotifierIsExpanded = ValueNotifier(true);
+
+  static String customDataTime(DateTime date) {
+    return '${date.year}-${twoCipher(date.month)}-${twoCipher(date.day)} ${twoCipher(date.hour)}:00';
+  }
+
+  static String twoCipher(int val) {
+    return val.toString().padLeft(2, '0');
+  }
 }
